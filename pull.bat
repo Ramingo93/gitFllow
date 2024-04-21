fde8e6e7c4977f5dd19d0695533d67153d5a0173
Ramingo93 <kklw93games@gmail.com>
2024-04-22 00:32:29 +0200

    starting points

---
+++ pull.bat
@@ -0,0 +1,37 @@
+
+
+:: create a template main.py 
+
+@echo off
+::https://stackoverflow.com/questions/21660249/how-do-i-make-one-particular-line-of-a-batch-file-a-different-color-then-the-oth
+SETLOCAL EnableDelayedExpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do     rem"') do (
+  set "DEL=%%a"
+)
+::______________________________________________________________________________
+
+if exist .git\ (
+	call :colorEcho 0e "GIT repo already available"
+	echo.
+	git status
+	echo.
+	set /p scelta="Step over to commit all? [Y/y]"
+	echo git commit
+	pause
+	exit 
+)
+
+git init
+call :colorEcho 0a "new GIT repo generated"
+echo.
+set /p scelta="Task finished..."
+#goto :eof
+#exit
+
+::______________________________________________________________________________
+:colorEcho
+echo off
+<nul set /p ".=%DEL%" > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1i
+goto :eof
